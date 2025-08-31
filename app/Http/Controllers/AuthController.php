7ad08f7de6e20c5ec7635a2414a0ffd600d2e1d9
amit <amitsarker95@gmail.com>
2025-08-31 12:22:19 +0600

    added login function in AuthController and define login route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Support\Str;
 
 class AuthController extends Controller
@@ -12,7 +12,7 @@ class AuthController extends Controller
     public function register(Request $request){
         $data = $request->validate([
             'name' => 'required|string|max:255',
-            'password' => 'required|string|max:6',
+            'password' => 'required|string|min:6',
             'role'=> 'required|in:admin,teacher,student',
         ]);
 
@@ -27,5 +27,21 @@ class AuthController extends Controller
 
         return response()->json(['Message' => 'User created Successfully !', 'token' => $user->api_token, 'user' => $user]);
         
+    }
+
+    public function login(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('name', $data['name'])->first();
+        if(!$user || !Hash::check($data['password'], $user->password)){
+            return response()->json(['invalid'=> 'User is not found'], 401);
+        }
+
+        $user->api_token = Str::random(60);
+        $user->save();
+        return response()->json(['api_token'=> $user->api_token, 'user' => $user], 200);
     }
 }
